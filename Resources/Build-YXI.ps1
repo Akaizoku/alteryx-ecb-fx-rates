8b3614fd9a4689e236875086e338a31a05ba7cd4
Florian Carrier <Akaizoku@users.noreply.github.com>
2025-12-18 13:52:14 +0100

    Restructure to build YXI
    Add script to build Alteryx package

---
+++ Resources/Build-YXI.ps1
@@ -0,0 +1,48 @@
+# ------------------------------------------------------------------------------
+# * Modules
+# ------------------------------------------------------------------------------
+Import-Module -Name "PSAYX"
+
+# ------------------------------------------------------------------------------
+# * Parameters
+# ------------------------------------------------------------------------------
+$Name               = "ECB Euro FX Reference Rates" # Name of the package
+$Author             = "Florian CARRIER"             # Author
+$Version            = "1.0.0"                       # Version number
+$CategoryName       = "Connectors"                  # Tool category
+$Description        = "This macro fetches euro foreign exchange references rates from the European Central Bank (ECB) for a specified period and list of currencies."
+$Icon               = ".\icon.png"                  # Package icon
+$CompressionLevel   = "Optimal"                     # YXI compression level
+
+# ------------------------------------------------------------------------------
+# * Prepare package content
+# ------------------------------------------------------------------------------
+# Create temporary package folder
+$Root    = Split-Path -Parent $PSScriptRoot
+$Package = Join-Path -Path $Root -ChildPath "YXI"
+New-Item -Path $Package -ItemType "Directory" -Force | Out-Null
+# Select files to include in the package
+$ExcludeDirs  = @(".git", "Resources", "YXI")
+$ExcludeExact = @("README.md", "LICENSE")
+$ExcludeLike  = @("*.bak", ".git*", "*.yxi")
+Get-ChildItem -Path $Root -Force |
+  Where-Object {
+    $ItemName = $PSItem.Name
+    $ItemName -notin $ExcludeDirs -and
+    $ItemName -notin $ExcludeExact -and
+    -not ($ExcludeLike | Where-Object { $ItemName -like $PSItem })
+  } |
+  Copy-Item -Destination $Package -Recurse -Force
+Copy-Item -Path $Icon -Destination "$Package\icon.png" -Force
+# Resolve package path
+$Path = Resolve-Path -Path $Package
+
+# ------------------------------------------------------------------------------
+# * Build YXI
+# ------------------------------------------------------------------------------
+New-AlteryxPackage -Path $Path -Name $Name -Author $Author -Version $Version -CategoryName $CategoryName -Description $Description -Icon "icon.png" -CompressionLevel $CompressionLevel -Unattended
+
+# ------------------------------------------------------------------------------
+# * Clean-up
+# ------------------------------------------------------------------------------
+Remove-Item -Path $Path -Recurse -Force
